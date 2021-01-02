3d1eadfc5832cafd84d6f00b619d4c20cd4e0432
zulvanF <zulvanfahmi1612@gmail.com>
2021-01-02 19:14:53 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,6 +8,11 @@
 <body>
 	<h1>hello</h1>
 	<p>Lorem ipsum, dolor sit amet, consectetur adipisicing elit. Eum dignissimos cum doloremque ad hic minus blanditiis odit unde aliquid voluptas optio tempore porro, fugit, sit necessitatibus nesciunt non exercitationem at.</p>
+	<ul>
+		<li>1</li>
+		<li>2</li>
+		<li>3</li>
+	</ul>
 	
 </body>
 </html>
